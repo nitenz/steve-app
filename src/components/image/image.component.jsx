42369547402eb57d8a2241294bce594a1181628d
nitenz <nitenz@gmail.com>
2019-10-02 00:04:45 +0100

    Added menu nav bar, implemented the zoom on the photos

--- src/components/image/image.component.jsx
+++ src/components/image/image.component.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 import './image.styles.scss';
 
-const Image = ( {imageUrl, imageAlt} ) => {
-    return(
-        <div className="image-container" >
-            <img src={imageUrl} alt={imageAlt}  />
-        </div>
-    );
-};
+class Image extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isImageFullScreen: false
+        } 
+    }
+
+    handleClick = (evt ) =>{
+        this.setState({isImageFullScreen: !this.state.isImageFullScreen});
+        console.log( 'isImageFullScreen: ', this.state.isImageFullScreen );
+    }
+
+    render(){
+        const { imageUrl, imageAlt } = this.props;
+
+        return(
+            <div className="image-container">
+                <img  className={`${this.state.isImageFullScreen ? 'fullscreen-image' : '' }`}  src={imageUrl} alt={imageAlt} onClick={ this.handleClick.bind( this ) }  />
+            </div> 
+        );
+    }
+
+}
 
 export default Image;
--- src/pages/photos/photos.component.jsx
+++ src/pages/photos/photos.component.jsx
@@ -10,15 +10,15 @@ class Photos extends React.Component{
 
         this.state ={
             photos: [
-                {url: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',alt:'tst1'},
-                {url:'https://i.ibb.co/ypkgK0X/blue-beanie.png',alt:'tst2'},
-                {url:'https://i.ibb.co/QdJwgmp/brown-cowboy.png',alt:'tst3'},
-                {url:'https://i.ibb.co/RjBLWxB/grey-brim.png',alt:'tst4'},
-                {url:'https://i.ibb.co/YTjW3vF/green-beanie.png',alt:'tst5'},
-                {url:'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',alt:'tst6'},
-                {url:'https://i.ibb.co/bLB646Z/red-beanie.png',alt:'tst7'},
-                {url:'https://i.ibb.co/1f2nWMM/wolf-cap.png',alt:'tst8'},
-                {url:'https://i.ibb.co/X2VJP2W/blue-snapback.png',alt:'tst9'}
+                {id: 1, url: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',alt:'tst1'},
+                {id: 2, url:'https://i.ibb.co/ypkgK0X/blue-beanie.png',alt:'tst2'},
+                {id: 3, url:'https://i.ibb.co/QdJwgmp/brown-cowboy.png',alt:'tst3'},
+                {id: 4, url:'https://i.ibb.co/RjBLWxB/grey-brim.png',alt:'tst4'},
+                {id: 5, url:'https://i.ibb.co/YTjW3vF/green-beanie.png',alt:'tst5'},
+                {id: 6, url:'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',alt:'tst6'},
+                {id: 7, url:'https://i.ibb.co/bLB646Z/red-beanie.png',alt:'tst7'},
+                {id: 8, url:'https://i.ibb.co/1f2nWMM/wolf-cap.png',alt:'tst8'},
+                {id: 9, url:'https://i.ibb.co/X2VJP2W/blue-snapback.png',alt:'tst9'}
             ]
         }
     }
@@ -28,7 +28,7 @@ class Photos extends React.Component{
             <div className="photos-container">
                 { this.state.photos.map(
                         ( item ) => (
-                            <Image imageUrl={ item.url} imageAlt={ item.alt } ></Image>
+                            <Image key={item.id} imageUrl={ item.url} imageAlt={ item.alt } ></Image>
                         )
                  )}
             </div>
